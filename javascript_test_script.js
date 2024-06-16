53a8570c7fce7936af16f5cba8c8e4f849be617b
CO_DE_PILOT <piratosnogi@gmail.com>
2024-06-16 20:11:41 +0200

    fetch practice

---
+++ javascript_test_script.js
@@ -0,0 +1,32 @@
+function download(url, filename) {
+  fetch(url, {
+    mode: "no-cors",
+    /*
+        * ALTERNATIVE MODE {
+        mode: 'cors'
+        }
+        *
+        */
+  })
+    .then((transfer) => {
+      return transfer.blob(); // RETURN DATA TRANSFERED AS BLOB
+    })
+    .then((bytes) => {
+      let elm = document.createElement("a"); // CREATE A LINK ELEMENT IN DOM
+      elm.href = URL.createObjectURL(bytes); // SET LINK ELEMENTS CONTENTS
+      elm.setAttribute("download", filename); // SET ELEMENT CREATED 'ATTRIBUTE' TO DOWNLOAD, FILENAME PARAM AUTOMATICALLY
+      elm.click(); // TRIGGER ELEMENT TO DOWNLOAD
+    })
+    .catch((error) => {
+      console.log(error); // OUTPUT ERRORS, SUCH AS CORS WHEN TESTING NON LOCALLY
+    });
+}
+
+/*
+ * @CALL
+ * EXAMPLE LOCAL FUNCTION CALL
+ */
+download(
+  "https://odp.met.hu/weather/weather_reports/synoptic/hungary/10_minutes/csv/HABP_10M_SYNOP_20240615173803.csv.zip",
+  "newtest.zip"
+);
