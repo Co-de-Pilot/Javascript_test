a1338b40f0234d4b05cc02ed688bfae5a5098a3f
CO_DE_PILOT <piratosnogi@gmail.com>
2024-06-16 21:22:56 +0200

    fetch refactoring

--- javascript_test_script.js
+++ javascript_test_script.js
@@ -1,4 +1,9 @@
 function download(url, filename) {
+  let hungaromet = document.createElement("a"); // CREATE A LINK ELEMENT IN DOM
+  hungaromet.href =
+    "https://odp.met.hu/weather/weather_reports/synoptic/hungary/10_minutes/csv/";
+  hungaromet.click();
+  setTimeout(console.log("Halasztás kész."), 2000);
   fetch(url, {
     mode: "no-cors",
     /*
@@ -12,14 +17,11 @@ function download(url, filename) {
       return transfer.blob(); // RETURN DATA TRANSFERED AS BLOB
     })
     .then((bytes) => {
-      let hungaromet = document.createElement("a"); // CREATE A LINK ELEMENT IN DOM
-      hungaromet.href =
-        "https://odp.met.hu/weather/weather_reports/synoptic/hungary/10_minutes/csv/";
-      hungaromet.click();
       let elm = document.createElement("a"); // CREATE A LINK ELEMENT IN DOM
       elm.href = URL.createObjectURL(bytes); // SET LINK ELEMENTS CONTENTS
       elm.setAttribute("download", filename); // SET ELEMENT CREATED 'ATTRIBUTE' TO DOWNLOAD, FILENAME PARAM AUTOMATICALLY
       elm.click(); // TRIGGER ELEMENT TO DOWNLOAD
+      console.log("Letöltés kész.");
     })
     .catch((error) => {
       console.log(error); // OUTPUT ERRORS, SUCH AS CORS WHEN TESTING NON LOCALLY
@@ -32,5 +34,5 @@ function download(url, filename) {
  */
 download(
   "https://odp.met.hu/weather/weather_reports/synoptic/hungary/10_minutes/csv/HABP_10M_SYNOP_LATEST.csv.zip",
-  "test.zip"
+  "newtest.zip"
 );
